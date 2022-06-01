83f8981432a2b0e2f26edcfe5839267fe8c63995
Ryan Nazareth <ryankarlos@gmail.com>
2022-06-01 20:39:16 +0100

    update docker image container example

--- examples/deploy-lambda-image/main_twitter.py
+++ examples/deploy-lambda-image/main_twitter.py
@@ -4,33 +4,28 @@ def handler(event, context):
     import itertools
     import boto3
     import json
-    if isinstance(event, str):
-        params = json.loads(event)["CodePipeline.job"]["data"]["actionConfiguration"][
-            "configuration"
-        ]["UserParameters"]
+    print(f"Event payload type: {type(event)}")
+    print(f"Event:{event}")
+    if event.get("CodePipeline.job") is not None:
+        mode = "cloud"
     else:
+        mode = "local"
+    # special parsing strategy for event from code pipeline as structure is nested differently -
+    # parameter stored in UserParameters key as str. So need extra step to convert this to dict
+    if mode == "cloud":
+        data = event["CodePipeline.job"]["data"]["actionConfiguration"]["configuration"]["UserParameters"]
+        params = json.loads(data)
+        job_id = event["CodePipeline.job"]["id"]
+        print(f"Params:{params}, JobID: {job_id}")
+    # Executing from local_run.py or passing in event for testing lambda function invocation via console
+    elif mode == "local":
         params = event.copy()
-    print(params)
-
+        print(f"Params:{params}")
     code_pipeline = boto3.client("codepipeline")
     response = get_secrets(mode="aws")
     api_keys = list(itertools.islice(response.values(), 4))
-    print(f"Event:{event}")
     print("Searching and delivering Tweets with Tweepy API: \n")
-    # special parsing strategy for event from code pipeline as structure is nested differently
-    # and of str type so need to convert to dict and parse data key
-    # Executing from local_run.py or passing in event for testing lambda function
-    # invocation via console - event passed to handler is already of dict type
-    try:
-        tweepy_search_api(params, *api_keys)
-        if params["mode"] == "cloud":
-            job_id = event["CodePipeline.job"]["id"]
-            code_pipeline.put_job_success_result(jobId=job_id)
-    except Exception as e:
-        print(str(e))
-        if params["mode"] == "cloud":
-            job_id = event["CodePipeline.job"]["id"]
-            code_pipeline.put_job_failure_result(
-                jobId=job_id, failureDetails={"message": str(e), "type": "JobFailed"}
-            )
-        raise
+
+    tweepy_search_api(params, *api_keys)
+    if mode == "cloud":
+        code_pipeline.put_job_success_result(jobId=job_id)
