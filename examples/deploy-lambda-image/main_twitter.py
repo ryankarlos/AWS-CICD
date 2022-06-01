771cfe35f0b92569529ee3d0f784e5b530f6fabb
Ryan Nazareth <ryankarlos@gmail.com>
2022-06-01 23:57:34 +0100

    add updates to cf templates and main twitter code for docker container

--- examples/deploy-lambda-image/main_twitter.py
+++ examples/deploy-lambda-image/main_twitter.py
@@ -4,16 +4,20 @@ def handler(event, context):
     import itertools
     import boto3
     import json
+
     print(f"Event payload type: {type(event)}")
     print(f"Event:{event}")
     if event.get("CodePipeline.job") is not None:
         mode = "cloud"
     else:
         mode = "local"
+    print(f"Mode: {mode}")
     # special parsing strategy for event from code pipeline as structure is nested differently -
     # parameter stored in UserParameters key as str. So need extra step to convert this to dict
     if mode == "cloud":
-        data = event["CodePipeline.job"]["data"]["actionConfiguration"]["configuration"]["UserParameters"]
+        data = event["CodePipeline.job"]["data"]["actionConfiguration"][
+            "configuration"
+        ]["UserParameters"]
         params = json.loads(data)
         job_id = event["CodePipeline.job"]["id"]
         print(f"Params:{params}, JobID: {job_id}")
@@ -26,6 +30,14 @@ def handler(event, context):
     api_keys = list(itertools.islice(response.values(), 4))
     print("Searching and delivering Tweets with Tweepy API: \n")
 
-    tweepy_search_api(params, *api_keys)
-    if mode == "cloud":
-        code_pipeline.put_job_success_result(jobId=job_id)
+    try:
+        tweepy_search_api(params, *api_keys)
+        if mode == "cloud":
+            code_pipeline.put_job_success_result(jobId=job_id)
+    except Exception as e:
+        print(f"Exception:{str(e)}")
+        if mode == "cloud":
+            code_pipeline.put_job_failure_result(
+                jobId=job_id, failureDetails={"message": str(e), "type": "JobFailed"}
+            )
+        raise
